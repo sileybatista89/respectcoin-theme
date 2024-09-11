bcb41986a636ff34765f9d12b6ded677f405c7ef
sileybatista89 <sileybatista89@gmail.com>
2024-09-11 17:22:33 +0100

    Create tokenomics.php

---
+++ tokenomics.php
@@ -0,0 +1,9 @@
+section id="tokenomics">
+    <h2>Tokenomics e Distribuição</h2>
+    <ul>
+        <li>Total Supply: 1.000.000.000 tokens</li>
+        <li>Liquidez e Listagem: 35%</li>
+        <li>Marketing e Parcerias: 20%</li>
+        <!-- Continue com as outras distribuições -->
+    </ul>
+</section>
