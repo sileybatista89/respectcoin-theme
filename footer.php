3fc95e9d8f08062382dbcdab0efbd5b13d96e220
sileybatista89 <sileybatista89@gmail.com>
2024-09-11 17:18:40 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p>&copy; 2024 RespectCoin. Todos os direitos reservados.</p>
+    <p>"Enquanto a cor da pele for mais importante que o brilho dos olhos, haverá guerra."</p>
+    <?php wp_footer(); ?>
+</footer>
+</body>
+</html>
