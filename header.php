ec642c3fbe95a2edc0358e1401ed04cdfdb11b44
sileybatista89 <sileybatista89@gmail.com>
2024-09-11 17:17:00 +0100

    header.php

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+<header>
+    <div class="logo">
+        <h1><?php bloginfo('name'); ?></h1>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="#roadmap">Roadmap</a></li>
+            <li><a href="#tokenomics">Tokenomics</a></li>
+        </ul>
+    </nav>
+</header>
