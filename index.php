a8d05df7d2c870f2bf39d82357b970a3d4f525bd
sileybatista89 <sileybatista89@gmail.com>
2024-09-11 16:13:48 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<main id="content">
+    <section id="intro">
+        <h1>Bem-vindo à RespectCoin</h1>
+        <p>Uma criptomoeda dedicada ao combate ao racismo.</p>
+    </section>
+
+    <?php include('roadmap.php'); ?>
+    <?php include('tokenomics.php'); ?>
+</main>
+
+<?php get_footer(); ?>
